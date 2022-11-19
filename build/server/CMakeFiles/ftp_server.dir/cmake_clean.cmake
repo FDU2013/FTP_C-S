22936961f72fba062e33f460d1d2c84b93dd0f73
Junfeifeifei <471612627@qq.com>
2022-11-19 11:23:23 +0800

    bugfix

---
+++ build/server/CMakeFiles/ftp_server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../../dir_server/ftp_server"
+  "../../dir_server/ftp_server.pdb"
+  "CMakeFiles/ftp_server.dir/server_ftp.c.o"
+  "CMakeFiles/ftp_server.dir/src/common.c.o"
+  "CMakeFiles/ftp_server.dir/src/file_transfer.c.o"
+  "CMakeFiles/ftp_server.dir/src/server_command.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ftp_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
